8d318752b04682b7cd1088fb235e902b82f5b6b8
吴铭 <451517996@qq.com>
2016-10-07 14:42:52 +0800

    c++ client扩展

---
+++ common/module/c++/module.h
@@ -0,0 +1,47 @@
+/*
+ * module
+ *
+ *  Created on: 2016-10-6
+ *      Author: qianqians
+ */
+#ifndef _module_h
+#define _module_h
+
+#include <map>
+#include <string>
+#include <functional>
+#include <vector>
+
+#include <boost/any.hpp>
+
+namespace common
+{
+
+class imodule
+{
+public:
+    imodule(){
+    }
+    
+    void reg_func(std::string func_name, std::function<void<std::vector<boost::any> > > func)
+    {
+        maps.insert(std::make_pair(func_name, func))
+    }
+    
+    void process_method(std::string func_name, std::vector<boost::any> argvs)
+    {
+        if (maps.has_key(func_name)){
+            maps[func_name](argvs);
+        }
+    }
+    
+private:
+    std::map<std::string, std::function<void<std::vector<boost::any> > > > maps;
+    
+    
+};
+
+}
+
+
+#endif //_client_h
